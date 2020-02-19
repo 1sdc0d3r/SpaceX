a3e623b07bb9e466690c573c467fb55429b36851
Braden <fish2355@hotmail.com>
2020-02-19 00:41:00 -0700

    added client

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-require("morgan")("combined");
-require("helmet")();
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 
 const server = express();
+server.use(morgan("combined"));
+server.use(cors()); //allow cross-origin
 
 server.use(
   "/graphql",
